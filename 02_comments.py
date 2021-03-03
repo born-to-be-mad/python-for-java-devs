97eadf8dabed160610678f6aa9819df7fa192308
Dzmitry Marudau <vinmaster@tut.by>
2021-03-03 23:54:48 +0300

    Python: comments

---
+++ 02_comments.py
@@ -0,0 +1,16 @@
+# Simple line comment
+#More comments
+
+print("# is used for commenting in python")
+"""
+print("multiline comment via "")
+print("multiline comment via "")
+print("multiline comment via "")
+"""
+
+'''
+print("# multiline comment via '")
+print("# multiline comment via '")
+'''
+
+
