c56592da258d74cd30ce2c49cd85fc0b040eae27
Dzmitry Marudau <vinmaster@tut.by>
2021-03-04 00:02:42 +0300

    Python: input data from console

---
+++ 03_user_input.py
@@ -0,0 +1,10 @@
+# Input data from console
+
+# use raw_input in python 2.7
+name = input("What is your name?")
+print("Hi " + name + ". What is your age?")
+age = int(input())
+print("You are " + name + " " + str(age) + "  years old.  What is you height?")
+height = float(input())
+
+print("You are " + name + " " + str(age) + "  years old and your height is " + str(height))
